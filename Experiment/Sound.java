d8b885736e9da4bb0798ed55e63963bf640d4ecd
Anthony Bao <anthonybao1999@gmail.com>
2016-07-02 20:07:40 -0700

    Update Sound.java

--- Experiment/Sound.java
+++ Experiment/Sound.java
@@ -1,42 +1,34 @@
-/*    */ package Experiment;
-/*    */ 
-/*    */ import java.applet.Applet;
-/*    */ import java.applet.AudioClip;
-/*    */ 
-/*    */ public class Sound
-/*    */ {
-/*  7 */   public static final Sound sound1 = new Sound("/tune3.wav");
-/*    */   private AudioClip clip;
-/*    */ 
-/*    */   public Sound(String filename)
-/*    */   {
-/*    */     try
-/*    */     {
-/* 11 */       this.clip = Applet.newAudioClip(Sound.class.getResource(filename));
-/*    */     } catch (Exception e) {
-/* 13 */       e.printStackTrace();
-/*    */     }
-/*    */   }
-/*    */ 
-/*    */   public void play() {
-/*    */     try {
-/* 18 */       new Thread() {
-/*    */         public void run() {
-/* 20 */           Sound.this.clip.loop();
-/*    */         }
-/*    */       }
-/* 22 */       .start();
-/*    */     } catch (Exception ex) {
-/* 24 */       ex.printStackTrace();
-/*    */     }
-/*    */   }
-/*    */ 
-/*    */   public static void main(String[] args) {
-/* 29 */     sound1.play();
-/*    */   }
-/*    */ }
+package Experiment;
 
-/* Location:           /Users/timcoker/Desktop/PartyBalls.jar
- * Qualified Name:     Experiment.Sound
- * JD-Core Version:    0.6.0
- */
+import java.applet.Applet;
+import java.applet.AudioClip;
+
+public class Sound  {
+  public static final Sound sound1 = new Sound("/tune3.wav");
+  private AudioClip clip;
+
+  public Sound(String filename) {
+    try {
+      this.clip = Applet.newAudioClip(Sound.class.getResource(filename));
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
+
+  public void play() {
+    try {
+      new Thread() {
+        public void run() {
+          Sound.this.clip.loop();
+        }
+      }
+      .start();
+    } catch (Exception ex) {
+      ex.printStackTrace();
+    }
+  }
+
+  public static void main(String[] args) {
+    sound1.play();
+  }
+}
